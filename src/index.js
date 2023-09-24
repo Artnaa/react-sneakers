a1ec4d94beea9bee0a95e2bec66d8161f7852aad
Artnaa <artnaa3@gmail.com>
2023-09-24 12:29:36 +0300

    some changes in index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Favorites from './pages/Favorites';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <App/>
- <Favorites/>
+  <App/>
+//  <Favorites/>
  
 );
